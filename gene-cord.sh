a49da440b5cba7e8ccf3905031f620b27f043b90
Mohammed Farahat <138137237+Mo7ammedFarahat@users.noreply.github.com>
2024-10-25 11:55:03 +0200

    Update gene-cord.sh

--- gene-cord.sh
+++ gene-cord.sh
@@ -1,7 +1,7 @@
-GName = 'your gene name'
+GName = "your gene name"
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/gencode.v47.basic.annotation.gff3.gz
-zgrep $GName gencode.v47.basic.annotation.gff3.gz |
+zgrep "'$GName'" gencode.v47.basic.annotation.gff3.gz |
     awk '$1 == "chr2" && $3 == "gene"' |
     grep gene_type=protein |
     awk -F'\t' '{split($9,a,";"); for(i in a) if(a[i]~/gene_name/) {split(a[i],b,"="); name=b[2]} print $1"\t"$4"\t"$5"\t"name}' | bedtools merge -d 15000 |
